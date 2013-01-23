fa963518a3f712f5ee4c3ecb79b23713d8134fd1
Thomas J Bradley <theman@thomasjbradley.ca>
2013-01-23 07:56:20 -0500

    Renamed rsync exclude file to be more descriptive.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rsync -e ssh -a --delete --progress --compress --exclude-from './exclude.txt' ./ ghdca:/home/ghdca/public_html
-rsync -e ssh -a --delete --progress --compress --exclude-from './exclude.txt' ./ ghdcom:/home/ghdcom/public_html
+rsync -e ssh -a --delete --progress --compress --exclude-from './rsync-exclude.txt' ./ ghdca:/home/ghdca/public_html
+rsync -e ssh -a --delete --progress --compress --exclude-from './rsync-exclude.txt' ./ ghdcom:/home/ghdcom/public_html
