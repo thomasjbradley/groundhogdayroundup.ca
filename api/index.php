5ebb1e1656e33f4311f10eb2898be2e29f14dfdb
Thomas J Bradley <theman@thomasjbradley.ca>
2013-01-25 06:58:28 -0500

    Added the correct mime-type for the API calls.

--- api/index.php
+++ api/index.php
@@ -5,6 +5,8 @@ require_once '../utils.php';
 $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
 $request = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_NUMBER_INT);
 
+header('Content-Type: application/json; charset=utf-8');
+
 if ($_SERVER['REQUEST_METHOD'] != 'GET') {
   header('HTTP/1.1 405 Method Not Allowed');
   echo '{"error":"Gopher’s can’t be manipulated."}';
