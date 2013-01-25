93f194fad5e1697acd3df518f4123d5229ce74c1
Thomas J Bradley <theman@thomasjbradley.ca>
2013-01-24 19:28:35 -0500

    Added a super awesome API.

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../utils.php';
+
+$host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+$request = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_NUMBER_INT);
+
+if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+  header('HTTP/1.1 405 Method Not Allowed');
+  echo '{"error":"Gopher’s can’t be manipulated."}';
+  exit;
+}
+
+if (!empty($request)) {
+  $year = getYear();
+  echo json_encode(getGopherList($year), JSON_UNESCAPED_SLASHES);
+} else {
+  $years = getAllYears();
+  $yearsJson = array();
+
+  foreach ($years as $year) {
+    $yearsJson[] = $host . $year;
+  }
+
+  echo json_encode($yearsJson, JSON_UNESCAPED_SLASHES);
+}
