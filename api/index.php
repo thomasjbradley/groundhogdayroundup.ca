28288dd859b519bfd586cc90ab8e80dbbc2f0b72
Thomas J Bradley <theman@thomasjbradley.ca>
2013-01-30 08:13:25 -0500

    Updated the API to support a /gophers call.

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 require_once '../utils.php';
 
 $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
-$request = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_NUMBER_INT);
+$request = preg_replace('@/*$@', '', str_replace('/api', '', filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_STRING)));
 
 header('Content-Type: application/json; charset=utf-8');
 
@@ -13,18 +13,28 @@ if ($_SERVER['REQUEST_METHOD'] != 'GET') {
   exit;
 }
 
-if (!empty($request)) {
-  $year = getYear();
-  // echo json_encode(getGopherList($year), JSON_UNESCAPED_SLASHES);
-  echo stripslashes(json_encode(getGopherList($year)));
-} else {
-  $years = getAllYears();
-  $yearsJson = array();
+switch ($request) {
+  case '':
+    $years = getAllYears();
+    $response = array(
+      'gophers' => $host . 'gophers'
+      , 'years' => array()
+    );
 
-  foreach ($years as $year) {
-    $yearsJson[] = $host . $year;
-  }
+    foreach ($years as $year) {
+      $response['years'][] = $host . $year;
+    }
 
-  // echo json_encode($yearsJson, JSON_UNESCAPED_SLASHES);
-  echo stripslashes(json_encode($yearsJson));
+    // echo json_encode($yearsJson, JSON_UNESCAPED_SLASHES);
+    echo stripslashes(json_encode($response));
+    break;
+
+  case '/gophers':
+    echo file_get_contents(GOPHERS_PATH);
+    break;
+
+  default:
+    $year = getYear();
+    // echo json_encode(getGopherList($year), JSON_UNESCAPED_SLASHES);
+    echo stripslashes(json_encode(getGopherData($year)));
 }
