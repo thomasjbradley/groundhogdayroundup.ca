61e9a421197fb1e9d261123150255d153a698c93
Thomas J Bradley <hey@thomasjbradley.ca>
2014-03-30 15:36:23 -0400

    Starting transition to GitHub hosting.

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-stylus -c -u nib css/groundhog-day-roundup.styl
-md5sum=`md5 -q css/groundhog-day-roundup.css`
-
-mv css/groundhog-day-roundup.css css/groundhog-day-roundup.${md5sum}.css
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./build.sh
-
-rsync -e ssh -a --delete --progress --compress --exclude-from './rsync-exclude.txt' ./ ghdca:/home/ghdca/public_html
-rsync -e ssh -a --delete --progress --compress --exclude-from './rsync-exclude.txt' ./ ghdcom:/home/ghdcom/public_html
